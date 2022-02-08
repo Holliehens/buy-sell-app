12a77116691e3e274779c11d898a055ea2954bb1
umarkhan167 <77021740+umarkhan167@users.noreply.github.com>
2022-02-08 13:22:53 -0500

    Created basic templates for each routes

--- server.js
+++ server.js
@@ -37,11 +37,16 @@ app.use(express.static("public"));
 // Note: Feel free to replace the example routes below with your own
 const usersRoutes = require("./routes/users");
 const widgetsRoutes = require("./routes/widgets");
-
+const adminRoutes = require("./routes/admins");
+const messagesRoutes = require("./routes/messages");
+const favouritesRoutes = require("./routes/favourites");
 // Mount all resource routes
 // Note: Feel free to replace the example routes below with your own
 app.use("/api/users", usersRoutes(db));
 app.use("/api/widgets", widgetsRoutes(db));
+app.use("/api/admins", adminRoutes(db));
+app.use("/api/messages", messagesRoutes(db));
+app.use("/api/favourites", favouritesRoutes(db));
 // Note: mount other resources here, using the same pattern above
 
 // Home page

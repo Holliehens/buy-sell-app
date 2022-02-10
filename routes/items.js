97e6d186daddef367f97fb0886cf5994e9ee7ec4
vanlgo <goellevan@gmail.com>
2022-02-10 18:36:38 +0000

    creating css for item page

--- routes/items.js
+++ routes/items.js
@@ -9,19 +9,8 @@ module.exports = (db) => {
 
   // Renders entire items table
   router.get("/item", (req, res) => {
-    let query = `SELECT * FROM items`;
-    console.log(query);
-    db.query(query)
-      .then(data => {
-        const items = data.rows;
-        res.json({ items });
-      })
-      .catch(err => {
-        res
-          .status(500)
-          .json({ error: err.message });
-      });
-  });
+    res.render("item");
+  })
 
   // Renders single item page
   router.get("/item/:id", (req, res) => {

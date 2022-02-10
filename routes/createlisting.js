e8ac3b230e37dea182329a4aa0422e42a1840f5b
vanlgo <goellevan@gmail.com>
2022-02-10 20:50:42 +0000

    current routes for Vania

--- routes/createlisting.js
+++ routes/createlisting.js
@@ -14,13 +14,29 @@ module.exports = (db) => {
     res.render("createlisting");
   });
 
-  // Initializes item into database
-  router.post("/createlisting", (req, res) => {
-    /* Get user id
-  - Require: name, description, price, photo_url
-  - Optional: is item featured
-  - Create INSERT query with all above information into item table */
-  });
+ // Initializes item into database
+ router.post("/createlisting", (req, res) => {
+  /* Get user id
+- Require: name, description, price, photo_url
+- Optional: is item featured
+- Create INSERT query with all above information into item table */
+const item_id = req.session.items_id;
+const { description, price, photo_url } = req.body;
 
-  return router;
+let query = 'INSERT INTO items (id, description, price, photo_url) VALUES($1, $2, $3, $4)';
+console.log(query);
+db.query(query, [item_id, description, price, photo_url])
+  .then(data => {
+    const item = data.rows[0];
+    res.json({ item });
+  })
+  .catch(err => {
+    res
+      .status(500)
+      .json({ error: err.message });
+  });
+});
+
+
+return router;
 };

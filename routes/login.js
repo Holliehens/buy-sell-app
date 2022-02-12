9809803d950e958f1188d63a1e7af4dd339eea95
vanlgo <goellevan@gmail.com>
2022-02-12 16:40:39 +0000

    added login route

--- routes/login.js
+++ routes/login.js
@@ -1,4 +1,12 @@
-app.get('/login/:id', (req, res) => {
+const express = require('express');
+const router = express.Router();
+
+module.exports = (db) => {
+
+router.get('/login/:id', (req, res) => {
   req.session.user_id = req.params.id;
   res.redirect('/');
 });
+
+return router;
+};

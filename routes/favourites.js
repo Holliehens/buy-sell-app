2a399874c0a0d58be8f8f6a5823411dc4de3b98e
vanlgo <goellevan@gmail.com>
2022-02-12 08:31:55 +0000

    added POST/favourites for users to add favourites to database

--- routes/favourites.js
+++ routes/favourites.js
@@ -1,7 +1,6 @@
-//TEMPLATE FOR ROUTES FROM USERS.JS
 //WE WILL WORK OFF OF THIS
 const express = require('express');
-const router  = express.Router();
+const router = express.Router();
 
 module.exports = (db) => {
 
@@ -10,24 +9,67 @@ module.exports = (db) => {
     /* Get current user's id
     - Create SELECT query getting all entries in favourites table associated with user
     - Get all item ids from favourites found and render */
-    let query = `SELECT * FROM items`;
-    db.query(query)
-    .then(data => {
-      const templateVars = { items: data.rows, featured: data.rows.filter(x => x.featured) };
-      res.render("index", templateVars);
-    })
-    .catch(err => {
-      res
-        .status(500)
-        .json({ error: err.message });
-    });
+    const user_id = req.session.user_id;
+
+    let query = `SELECT *
+    FROM favourites
+    JOIN users ON user_id = users.id
+    WHERE user_id = $1;
+    `;
+
+    db.query(query, [user_id])
+      .then(data => {
+        const templateVars = { fav: data.rows };
+        console.log(templateVars);
+        res.render("favourites", templateVars);
+      })
+      .catch(err => {
+        res
+          .status(500)
+          .json({ error: err.message });
+      });
   });
 
   // add item to user's favourites
-  router.get("/favourites", (req, res) => {
+  router.post("/favourites/:id", (req, res) => {
     /* Get current user's id
     - Get item id
     - Create INSERT query to add into favourites table */
+    const user_id = req.session.user_id;
+    if (!user_id) {
+      return res
+        .status(401)
+        .json({ error: 'Please login' });
+    };
+
+    const item_id = req.params.id;
+
+    let query = `SELECT *
+    FROM items
+    WHERE id = $1
+    `;
+
+    db.query(query, [item_id])
+      .then(data => {
+        const item = data.rows[0];
+        let query = 'INSERT INTO favourites (user_id, item_id) VALUES($1, $2) RETURNING *';
+        db.query(query, [user_id, item.id])
+          .then(data => {
+            const favourite = data.rows[0];
+            console.log(favourite);
+            res.json({ favourite });
+          })
+          .catch(err => {
+            res
+              .status(500)
+              .json({ error: err.message });
+          });
+      })
+      .catch(err => {
+        res
+          .status(500)
+          .json({ error: err.message });
+      });
   });
 
   return router;

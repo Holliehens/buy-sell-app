2ece7c44cce39a54c2f77b349b9395dd38e339d8
umarkhan167 <77021740+umarkhan167@users.noreply.github.com>
2022-02-11 22:05:52 -0500

    Complete msg and convo routes

--- routes/favourites.js
+++ routes/favourites.js
@@ -1,11 +1,18 @@
-//TEMPLATE FOR ROUTES FROM USERS.JS
-//WE WILL WORK OFF OF THIS
+/*
+ * See: https://expressjs.com/en/guide/using-middleware.html#middleware.router
+ */
+
 const express = require('express');
 const router  = express.Router();
 
 module.exports = (db) => {
-  router.get("/", (req, res) => {
-    db.query(`SELECT * FROM users;`)
+
+  // display items that are in the current user's favourites
+  router.get("/:id", (req, res) => {
+    // res.send("hello")
+    const {id} = req.params;
+    let query = `SELECT * FROM favourites WHERE user_id = $1;`, [id];
+    db.query(query)
       .then(data => {
         const users = data.rows;
         res.json({ users });
@@ -15,6 +22,17 @@ module.exports = (db) => {
           .status(500)
           .json({ error: err.message });
       });
+    /* Get current user's id
+    - Create SELECT query getting all entries in favourites table associated with user
+    - Get all item ids from favourites found and render */
   });
+
+  // add item to user's favourites
+  router.post("/favourites", (req, res) => {
+    /* Get current user's id
+    - Get item id
+    - Create INSERT query to add into favourites table */
+  });
+
   return router;
 };
--- routes/messages.js
+++ routes/messages.js
@@ -1,14 +1,19 @@
-//TEMPLATE FOR ROUTES FROM USERS.JS
-//WE WILL WORK OFF OF THIS
+/*
+ * See: https://expressjs.com/en/guide/using-middleware.html#middleware.router
+ */
+
 const express = require('express');
-const router  = express.Router();
+const router = express.Router();
 
 module.exports = (db) => {
-  router.get("/", (req, res) => {
-    db.query(`SELECT * FROM users;`)
+
+  //Create new conversation
+  router.get("/messages", (req, res) => {
+    let query = `SELECT * FROM messages;`;
+    db.query(query)
       .then(data => {
-        const users = data.rows;
-        res.json({ users });
+        const messages = data.rows;
+        res.json({ messages });
       })
       .catch(err => {
         res
@@ -16,5 +21,46 @@ module.exports = (db) => {
           .json({ error: err.message });
       });
   });
+
+  router.get("/messages/:id", (req, res) => {
+    const {id} = req.params;
+    let query = `SELECT id, conversation_id FROM messages WHERE id = $1;`, [id];
+    db.query(query)
+      .then(data => {
+        const messages = data.rows;
+        res.json({ messages });
+      })
+      .catch(err => {
+        res
+          .status(500)
+          .json({ error: err.message });
+      });
+  });
+
+  // creates new message
+  router.post("/messages", (req, res) => {
+    /* Get current user id
+    - Get text of message
+    - Get date_posted
+    - Get conversation id
+    - Create INSERT query into messages table */
+    let query =`
+    INSERT INTO messages(sender_id, message, date_posted, conversation_id)
+    VALUES (${req.body})
+    RETURNING *;`;
+    db.query(query)
+      .then(data => {
+        const messages = data.rows;
+        res.json({ messages });
+      })
+      .catch(err => {
+        res
+          .status(500)
+          .json({ error: err.message });
+      });
+  });
+
   return router;
 };
+
+
